d08296c19bca34f205b45b47349094a512625643
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-04-02 21:22:29 +0100

    Working on sampling of environment

--- src/ppo/ppo.clj
+++ src/ppo/ppo.clj
@@ -1,14 +1,38 @@
 (ns ppo.ppo
     (:require
-      [ppo.environment :refer (environment-observation environment-update)]))
+      [ppo.environment :refer (environment-observation environment-update environment-reward environment-done?
+                               environment-truncate?)]))
+
+
+(defn done?
+  "Decide whether a run is finished or aborted"
+  [environment]
+  (or (environment-done? environment) (environment-truncate? environment)))
 
 
 (defn sample-environment
   "Collect trajectory data from environment"
   [environment-factory policy n]
-  (loop [environment (environment-factory) observations [] i n]
+  (loop [state             (environment-factory)
+         observations      []
+         next-observations []
+         rewards           []
+         dones             []
+         i                 n]
     (if (zero? i)
-      {:observations observations}
-      (let [observation (environment-observation environment)
-            action      (policy observation)]
-        (recur (environment-update environment action) (conj observations observation) (dec i))))))
+      {:observations      observations
+       :next-observations next-observations
+       :rewards           rewards
+       :dones             dones}
+      (let [observation      (environment-observation state)
+            reward           (environment-reward state)
+            done             (done? state)
+            action           (policy observation)
+            next-state       (if done (environment-factory) (environment-update state action))
+            next-observation (environment-observation next-state)]
+        (recur next-state
+               (conj observations observation)
+               (conj next-observations next-observation)
+               (conj rewards reward)
+               (conj dones done)
+               (dec i))))))
--- test/ppo/t_ppo.clj
+++ test/ppo/t_ppo.clj
@@ -11,16 +11,21 @@
   (environment-observation [_this] [(+ state 100)])
   (environment-done? [_this] (>= state 10))
   (environment-truncate? [_this] (< state 0))
-  (environment-reward [_this] (- 5 (abs (- state 5)))))
+  (environment-reward [_this] (- (abs (- state 5)))))
 
 (def counter (atom 0))
 
 
-(defn test-env-factory [] (let [counter (atom 0)] (fn [] (->TestEnvironment (swap! counter inc)))))
+(defn test-env-factory [] (constantly (->TestEnvironment 1)))
 (defn stop-at-102 [observation] (if (>= (first observation) 102) [0] [1]))
 
 (facts "Generate samples from environment"
        (:observations (sample-environment (test-env-factory) (constantly [0]) 1)) => [[101]]
        (:observations (sample-environment (test-env-factory) (constantly [0]) 2)) => [[101] [101]]
        (:observations (sample-environment (test-env-factory) (constantly [1]) 2)) => [[101] [102]]
-       (:observations (sample-environment (test-env-factory) stop-at-102 3)) => [[101] [102] [102]])
+       (:observations (sample-environment (test-env-factory) stop-at-102 3)) => [[101] [102] [102]]
+       (:rewards (sample-environment (test-env-factory) (constantly [1]) 5)) => [-4 -3 -2 -1 0]
+       (:dones (sample-environment (test-env-factory) (constantly [3]) 4)) => [false false false true]
+       (:observations (sample-environment (test-env-factory) (constantly [3]) 5)) => [[101] [104] [107] [110] [101]]
+       (:dones (sample-environment (test-env-factory) (constantly [-1]) 3)) => [false false true]
+       (:next-observations (sample-environment (test-env-factory) (constantly [3]) 5)) => [[104] [107] [110] [101] [104]])
