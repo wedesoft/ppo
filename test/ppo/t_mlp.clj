30ceae9b6f502f0684ead1595750a7bcc1ab3e85
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-04-14 12:50:10 +0100

    Try to remove use of tensor-indeterministic-act

--- test/ppo/t_mlp.clj
+++ test/ppo/t_mlp.clj
@@ -55,9 +55,9 @@
            (tolist (py. zero-actor deterministic_act (tensor [[0 0]]))) => [[0.0]]
            (tolist (py/py.- (py. zero-actor get_dist (tensor [[0 0]])) mean)) => [[0.0]]
            (tolist (py/py.- (py. zero-actor get_dist (tensor [[0 0]])) stddev)) => [[0.6931471824645996]]
-           (let [result ((tensor-indeterministic-act zero-actor) (tensor [[0 0]]))]
-             (tolist (:action result)) => some?
-             (tolist (:logprob result)) => some?
-             (tolist ((logprob-of-action zero-actor) (tensor [[0 0]]) (:action result))) => (tolist (:logprob result)))
+           (let [result ((indeterministic-act zero-actor) [[0 0]])]
+             (:action result) => some?
+             (:logprob result) => some?
+             (tolist ((logprob-of-action zero-actor) (tensor [[0 0]]) (tensor (:action result)))) => (:logprob result))
            (:action ((indeterministic-act zero-actor) [[0 0]])) => some?
            (:logprob ((indeterministic-act zero-actor) [[0 0]])) => some?)))
