cd16034f371da4371c87e7ceaf59419cecd42a98
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-04-14 23:54:21 +0100

    Save model

--- src/ppo/core.clj
+++ src/ppo/core.clj
@@ -1,10 +1,13 @@
 (ns ppo.core
     (:gen-class)
     (:require [clojure.math :refer (PI)]
+              [libpython-clj2.require :refer (require-python)]
               [libpython-clj2.python :refer (py.) :as py]
               [ppo.mlp :refer (Actor Critic adam-optimizer tolist)]
               [ppo.ppo :refer (sample-with-advantage-and-critic-target actor-loss critic-loss)]
               [ppo.pendulum :refer (->Pendulum config setup)]))
+
+(require-python '[torch :as torch])
 
 
 (defn pendulum-factory
@@ -18,12 +21,12 @@
         critic           (Critic 3 10)
         n                100
         c                50
-        actor-optimizer  (adam-optimizer actor 0.1 0.001)
-        critic-optimizer (adam-optimizer critic 0.1 0.001)]
+        actor-optimizer  (adam-optimizer actor 0.01 0.001)
+        critic-optimizer (adam-optimizer critic 0.01 0.001)]
     (doseq [k (range n)]
            (let [sum-actor-loss  (atom 0.0)
                  sum-critic-loss (atom 0.0)
-                 samples         (sample-with-advantage-and-critic-target factory actor critic 128 16 0.9 1.0)]
+                 samples         (sample-with-advantage-and-critic-target factory actor critic (* 64 32) 64 0.99 1.0)]
              (doseq [epoch (range c)]
                     (doseq [batch samples]
                            (py. actor-optimizer zero_grad)
@@ -38,5 +41,9 @@
                              (py. loss backward)
                              (py. critic-optimizer step)
                              (swap! sum-critic-loss + (tolist loss)))))
-             (println "Epoch: " k " Actor Loss: " (/ @sum-actor-loss c) " Critic Loss: " (/ @sum-critic-loss c))))
+             (println "Epoch:" k
+                      "Actor Loss:" (/ @sum-actor-loss c (count samples))
+                      "Critic Loss:" (/ @sum-critic-loss c (count samples)))))
+    (torch/save (py. actor state_dict) "actor.pt")
+    ; (py. actor load_state_dict (torch/load "actor.pt"))
     (System/exit 0)))
