dbf546256b8a7b36dd6fb2374db7d6979439aad6
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-04-15 20:14:15 +0100

    Fewer batches

--- src/ppo/core.clj
+++ src/ppo/core.clj
@@ -19,20 +19,24 @@
   (let [factory          pendulum-factory
         actor            (Actor 3 10 1)
         critic           (Critic 3 10)
-        n                5
+        n-epochs         20
         gamma            0.8
         lambda           1.0
         epsilon          0.2
+        batch-size       64
+        n-batches        16
+        checkpoint       10
         actor-optimizer  (adam-optimizer actor 0.01 0.001)
         critic-optimizer (adam-optimizer critic 0.01 0.001)]
-    (when (.exists (java.io.File. "actor.pt"))
-      (py. actor load_state_dict (torch/load "actor.pt")))
-    (when (.exists (java.io.File. "critic.pt"))
-      (py. critic load_state_dict (torch/load "critic.pt")))
-    (doseq [k (range n)]
+    ; (when (.exists (java.io.File. "actor.pt"))
+    ;   (py. actor load_state_dict (torch/load "actor.pt")))
+    ; (when (.exists (java.io.File. "critic.pt"))
+    ;   (py. critic load_state_dict (torch/load "critic.pt")))
+    (doseq [epoch (range n-epochs)]
            (let [smooth-actor-loss  (atom 0.0)
                  smooth-critic-loss (atom 0.0)
-                 samples         (sample-with-advantage-and-critic-target factory actor critic (* 64 32) 64 gamma lambda)]
+                 samples         (sample-with-advantage-and-critic-target factory actor critic (* batch-size n-batches)
+                                                                          batch-size gamma lambda)]
              (doseq [batch samples]
                     (py. actor-optimizer zero_grad)
                     (let [loss (actor-loss batch actor epsilon)]
@@ -45,10 +49,10 @@
                       (py. loss backward)
                       (py. critic-optimizer step)
                       (swap! smooth-critic-loss (fn [x] (+ (* 0.97 x) (* 0.01 (tolist loss)))))))
-             (println "Epoch:" k
+             (println "Epoch:" epoch
                       "Actor Loss:" @smooth-actor-loss
                       "Critic Loss:" @smooth-critic-loss))
-           (when (= (mod k 50) 49)
+           (when (= (mod epoch checkpoint) (dec checkpoint))
              (torch/save (py. actor state_dict) "actor.pt")
              (torch/save (py. critic state_dict) "critic.pt")))
     (torch/save (py. actor state_dict) "actor.pt")
